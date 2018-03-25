a8c6edcf5135216c4f549af92584e0e80f92c718
Dakota <RoboRoyal@users.noreply.github.com>
2018-03-25 02:22:31 -0400

    Add files via upload

---
+++ motorControle.java
@@ -0,0 +1,99 @@
+package robosub;
+
+//import org.apache.log4j.Logger;
+
+/**
+* Class to interface between direct motor control and update
+* @author Dakota
+*
+*/
+class motorControle {//implements Runnable{
+	public static int form = 0;
+	//private static Logger logger = Logger.getLogger(motorControle.class.getCanonicalName());
+	private static int TopSpeed = 1700; //software defined top speed
+	public static final int max_speed = 1750;//this is a very conservative max and min for the motors
+	public static final int min_speed = 1250;//too high or too low can danmage them. Hardware limit
+	//however, you can adjust if you need more power
+	//Actual max and min values of current motor servo controlers are 1100-1900
+	
+	//static boolean init = false;//old
+	static int[] motor_vals = {1500,1500,1500,1500,1500,1500};//FLM,FRM,BLM,BRM,LM,RM
+	static boolean[] motor_enable = {true, true, true, true, true, true};
+	private static boolean invertVerticalMotors = false;
+	//private static final int baseSpeed = 1500;
+
+	/**
+	 * Sets motor values, formats and passes values to update
+	 * @param motors Array of motor values, 2, 4, or 6 values 
+	 * @throws Exception Incorrect number of motor values
+	 */
+	public static void set_motors(int[] motors) throws Exception{
+		for(int i = 0;i<motors.length;i++){
+			if(motors[i] > TopSpeed){
+				motors[i] = TopSpeed;
+			}else if(motors[i] < 3000 - TopSpeed){
+				motors[i] = 3000 - TopSpeed;
+			}
+			/*if(motors[i] > max_speed){
+				debug.print("Warning; invalid motor value, value too high: "+motors[i] + " @ motor: "+i);
+				motors[i] = max_speed;
+			}else if(motors[i] < min_speed){
+				debug.print("Warning; invalid motor value, value too low: "+motors[i]+ " @ motor: "+i);
+				motors[i] = min_speed;
+			}*/
+		}
+		form++;
+		if(form%10==0 && basic.debug_lvl >= 9){System.out.println("Motors: "); for(int i = 0;i<motors.length;i++) System.out.print("Seting motor @: "+motors[i]);if(form>12300) form = 0;}
+		if(motors.length == 6){
+			//all motors
+			motor_vals = motors;
+		}else if(motors.length == 4){
+			//depth motors
+			for(int i=0;i<4;i++){
+				if(invertVerticalMotors){
+					motors[i] = 3000-motors[i];
+				}
+				motor_vals[i]=motors[i];
+			}
+		}else if(motors.length == 2){
+			//moving motors
+			for(int i=4;i<6;i++){
+				motor_vals[i]=motors[i-4];
+			}
+		}else{
+			if(core.no_fill()){
+				for(int i=0;i<6;i++){
+					if(invertVerticalMotors && i <=4){
+						motors[i] = 3000-motors[i];
+					}
+					motor_vals[i]=motors[i];
+				}
+			}else{
+				debug.error(motors.length+" motor values given, only 2,4 and 6 are valid");
+				throw new Exception("Invalid motor commands givent to motorControle.set_motors: "+motors);
+			}	
+		}
+		//possibly fix up this code to make it work better
+		for(int p = 0; p<6;p++){
+			if(!motor_enable[p]){
+				motor_vals[p] = 1500;//sets motor to off if its disable
+			}
+		}
+		update.set_motors(motor_vals);
+	}
+	public static void motor_enable(int mot, boolean en){
+		debug.print("Motor "+mot+" set to "+en);
+		motor_enable[mot] = en;
+	}
+	public static void setTopSpeed(int newSpeed){
+		if(newSpeed > max_speed){
+			System.out.println("Warning; invalid motor value, value too high.");
+			newSpeed = max_speed;
+		}
+		if(newSpeed < min_speed){
+			System.out.println("Warning; invalid motor value, value too low.");
+			newSpeed = min_speed;
+		}
+		TopSpeed = newSpeed;
+	}
+}
---
+++ sonar.java
@@ -0,0 +1,157 @@
+package robosub;
+
+//import org.apache.log4j.Logger;
+
+/**
+ * All things sonar, one I figure that out
+ * 
+ * @author Dakota
+ *
+ */
+class sonar implements Runnable {
+	//private static Logger logger = Logger.getLogger(sonar.class.getCanonicalName());
+	Thread t;
+	static int target_freq = 35000;
+	static int pinger_depth = 10;
+	static int pinger_dir = 0;
+	static int pinger_dist = 50;
+	private static boolean init = false;
+	private static boolean run = false;
+
+	public static String print() {
+		String re = "Sonar: ";
+		re += " Pinger diection: " + pinger_dir;
+		re += " Pinger distance: " + pinger_dist;
+		re += " Pinger depth " + pinger_depth;
+		re += " Target freq " + target_freq;
+		return re;
+	}
+
+	public String toString() {// self print
+		return print();
+	}
+	
+	public static void stop() {
+		run = false;
+	}
+
+	public static void set_target_freq(int freq) {
+		target_freq = freq;
+	}
+
+	public static int get_pinger_depth() {
+		if (!init) {
+			debug.error("Accessing sonar without first initiating 1 ");
+		}
+		return pinger_depth;
+	}
+
+	public static int get_pinger_dir() {
+		if (!init) {
+			debug.error("Accessing sonar without first initiating 2 ");
+		}
+		return pinger_dir;
+	}
+
+	public static int get_pinger_dist() {
+		if (!init) {
+			debug.error("Accessing sonar without first initiating 3 ");
+		}
+		return pinger_dist;
+	}
+
+	public static void sonar_calc_simple(int freq) {
+		// (left_in, right_in, top_in)
+		// first signal is at t=0, counting difference in time in seconds(or
+		// nano)
+		// int freq = 35000;//target freq
+		Long left_time = (long) 0, right_time = (long) 0, top_time = (long) 0;
+
+		/*
+		 * while(left_in == 0 || right_in == 0 || top_in == 0){ Update
+		 * (right_in, left_in,top_in);//gets time when freq is detected }
+		 */
+
+		int C = 1484; // speed of sound in water (m/s)
+		double dist2 = 1.0; // distance between microphones (meters)
+
+		double dir = Math.asin((left_time - right_time) * (C / dist2));
+		int dpth = (int) (((dir / 2) - top_time) * (C / dist2));
+
+		pinger_dir = (int) dir;
+		pinger_depth = dpth;
+		pinger_dist = (int) ((left_time + right_time + top_time) * (1 / 3) * (C / dist2));
+
+	}
+
+	private void sonar_run() {
+		while (run) {
+			// take pinger_freq
+			// update all pinger values
+			// repeat
+			sonar_calc_simple(target_freq);
+			try {
+				Thread.sleep(90);
+			} catch (Exception e) {
+				debug.logWithStack("Thread interupt");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Initilizing sonar");
+		// run set up to initiate sonar
+		// self test
+		if (sonar_init() && sonar_self_test()) {
+			run = true;
+			init = true;
+			System.out.println("Sonar initilizing compleate; NOT IMPLIMENTED!");
+			sonar_run();
+			System.out.println("Shutting down sonar");
+		} else {
+			System.out.println("Problem initiating or running sonar");
+		}
+
+	}
+
+	private boolean sonar_self_test() {
+		/*try {
+			double p = update.sonar_dir(0);
+			Thread.sleep(200);
+			if (p != update.sonar_dir(0)) {
+				return false;
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+		return true;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private boolean sonar_init() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public void start() {
+		if (t == null) {
+			t = new Thread(this, "sonar");
+			t.start();
+		}else{
+       	 debug.logWithStack("Second instance being made: update");
+        }
+	}
+}
