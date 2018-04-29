684f8decf8439dfea44c4a4c8fcb71be26bf1637
Dakota <RoboRoyal@users.noreply.github.com>
2018-04-29 02:17:14 -0400

    Add files via upload

--- SonarUtil/SonarExec.java
+++ SonarUtil/SonarExec.java
@@ -1,76 +1,122 @@
 package SonarUtil;
 
+import java.io.IOException;
+
 public class SonarExec implements Runnable {
 	private boolean RUN = false;
 	private Thread t;
-	//private pingerReader left, right;
+	// private pingerReader left, right;
 	static String left = "SonarUtil/left.txt";
 	static String right = "SonarUtil/right.txt";
-	
-	public static int light(){
+
+	public static int lighter() {
+		int dir = 0;
+		int bucket = 0;
+		try {
+			Process p = Runtime.getRuntime().exec("python3 lighterPy.py ");
+			// Process p2 = Runtime.getRuntime().exec("python3 right.py ");
+			p.waitFor();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Thread.sleep(100);
+		} catch (Exception e) {
+		}
+
+		int min = Sonar_Test.getSize(left);
+		if (Sonar_Test.size(left) > Sonar_Test.getSize(right))
+			min = Sonar_Test.getSize(right);
+		min = min - 1;
+		System.out.println("Min: " + min);
+		Search.left = Sonar_Test.readInSmall(left, min);
+		Search.right = Sonar_Test.readInSmall(right, min);
+		System.out.println("Got data; " + Search.left.length + ", " + Search.right.length);
+		try {
+			bucket = Search.findBucket();
+			System.out.println("Bucket is: " + bucket);
+		} catch (Exception e) {
+			System.out.println("Fail1: " + e);
+			e.printStackTrace();
+		}
+		try {
+			dir = Search.findDir(bucket);
+			System.out.println("Dir is: " + dir);
+		} catch (Exception e) {
+			System.out.println("Fail2: " + e);
+			e.printStackTrace();
+		}
+		return dir;
+	}
+
+	public static int light() {
 		int dir = 0;
 		int bucket = 0;
 		socketMe2 me = new socketMe2();
 		me.start();
 		System.out.println(me.send("start"));
 		me.waitEnd();
-		//System.out.println(me.send("close"));
-		//me.close();
-		try{Thread.sleep(100);}catch(Exception e){}
-		
+		// System.out.println(me.send("close"));
+		// me.close();
+		try {
+			Thread.sleep(100);
+		} catch (Exception e) {
+		}
+
 		int min = Sonar_Test.getSize(left);
-		if(Sonar_Test.size(left)>Sonar_Test.getSize(right))
+		if (Sonar_Test.size(left) > Sonar_Test.getSize(right))
 			min = Sonar_Test.getSize(right);
-		min = min -1;
+		min = min - 1;
 		System.out.println("Min: " + min);
-		Search.left=Sonar_Test.readInSmall(left,min);
-		Search.right=Sonar_Test.readInSmall(right,min);
-		System.out.println("Got data; "+Search.left.length+", "+Search.right.length);
-		try{
+		Search.left = Sonar_Test.readInSmall(left, min);
+		Search.right = Sonar_Test.readInSmall(right, min);
+		System.out.println("Got data; " + Search.left.length + ", " + Search.right.length);
+		try {
 			bucket = Search.findBucket();
-			System.out.println("Bucket is: "+bucket);
-		}catch(Exception e){
-			System.out.println("Fail1: "+e);
+			System.out.println("Bucket is: " + bucket);
+		} catch (Exception e) {
+			System.out.println("Fail1: " + e);
 			e.printStackTrace();
 		}
-		try{
+		try {
 			dir = Search.findDir(bucket);
-			System.out.println("Dir is: "+dir);
-		}catch(Exception e){
-			System.out.println("Fail2: "+e);
+			System.out.println("Dir is: " + dir);
+		} catch (Exception e) {
+			System.out.println("Fail2: " + e);
 			e.printStackTrace();
 		}
 		return dir;
 	}
 
-	public void norm(){
-		//first time
+	public void norm() {
+		// first time
 		//
-		
-		//LOOP:----------------
-		//connection
-		//wait for left
-		//start left reader
+
+		// LOOP:----------------
+		// connection
+		// wait for left
+		// start left reader
 		/*
-		 * -when left done done, search for bucket
-		 * -when right ready, start reading right. Verify correct version
+		 * -when left done done, search for bucket -when right ready, start
+		 * reading right. Verify correct version
 		 */
-		//when both are ready and bucket is found, start search
-		//...
-		//when search is done, update pinger connection
-		//END LOOP:-----------
-		
-		//close everything with end()
+		// when both are ready and bucket is found, start search
+		// ...
+		// when search is done, update pinger connection
+		// END LOOP:-----------
+
+		// close everything with end()
 	}
-	public void setup(){
-		//left = new pingerReader("dosent.exist", "LEFT");
-		//right = new pingerReader("dosent.exist2", "RIGHT");
-		//start py prog?
-		//set up socket connection
-		//test connect
+
+	public void setup() {
+		// left = new pingerReader("dosent.exist", "LEFT");
+		// right = new pingerReader("dosent.exist2", "RIGHT");
+		// start py prog?
+		// set up socket connection
+		// test connect
 	}
-	
-	
+
 	public void run() {
 		try {
 			norm();
@@ -78,7 +124,7 @@ public class SonarExec implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void start() {
 		if (!RUN) {
 			RUN = true;
@@ -87,7 +133,7 @@ public class SonarExec implements Runnable {
 				setup();
 				t.start();
 			} else {
-				System.out.println("Problem making SonarExec:"+t);
+				System.out.println("Problem making SonarExec:" + t);
 			}
 		} else {
 			System.out.println("Trying to make second instance of SonarExec");
